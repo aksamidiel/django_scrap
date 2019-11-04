cc55d4118898b8f2ea4e48681bb0ed644e2c7839
aksamidiel <38276155+aksamidiel@users.noreply.github.com>
2019-11-04 23:19:46 +0300

    initial

---
+++ core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Category(models.Model):
+    root_category = models.ForeignKey("self", blank=True, null=True)
+    name = models.CharField(max_length=255)
+    urls = models.TextField(blank=True)
+    sort = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["sort"]
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    category = models.ForeignKey("Category")
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0)
+    image = models.ImageField(blank=True, upload_to="product/")
+    descr = models.TextField(blank=True)
+    sort = models.IntegerField(default=0)
+    urls = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ["sort"]
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
